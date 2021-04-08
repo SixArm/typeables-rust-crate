b9729ecc12d3cec51950e960c2cf642c7d8b3dd9
Joel Parker Henderson <joel@joelparkerhenderson.com>
2021-04-08 12:12:08 -0700

    Add crate

---
+++ src/grammar/verb.rs
@@ -0,0 +1,16 @@
+/// Verb
+///
+/// https://wikipedia.org/wiki/Verb
+///
+/// A verb, from the Latin verbum meaning word, is a word (part of speech) that
+/// in syntax conveys an action (bring, read, walk, run, learn), an occurrence
+/// (happen, become), or a state of being (be, exist, stand). In the usual
+/// description of English, the basic form, with or without the particle to, is
+/// the infinitive. In many languages, verbs are inflected (modified in form) to
+/// encode tense, aspect, mood, and voice. A verb may also agree with the
+/// person, gender or number of some of its arguments, such as its subject, or
+/// object. Verbs have tenses: present, to indicate that an action is being
+/// carried out; past, to indicate that an action has been done; future, to
+/// indicate that an action will be done. 
+
+pub type Verb = String;
---
+++ src/lib.rs
@@ -0,0 +1,7 @@
+pub use self::grammar::noun::Noun;
+pub use self::grammar::verb::Verb;
+
+pub mod grammar {
+    pub mod noun;
+    pub mod verb;
+}
