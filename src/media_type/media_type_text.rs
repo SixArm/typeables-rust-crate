a86da4247f2a059df9f3f7e456782907fb07c20b
Joel Parker Henderson <joel@joelparkerhenderson.com>
2021-04-10 16:46:28 -0700

    Add MediaTypeText

---
+++ src/media_type/media_type_text.rs
@@ -0,0 +1,13 @@
+/// Media type text
+///
+/// # Examples
+///
+/// ```
+/// let x = MediaTypeText = "text/plain".into();
+/// let x = MediaTypeText = "image/jpeg".into();
+/// let x = MediaTypeText = "audio/ogg".into();
+/// let x = MediaTypeText = "video/mpeg".into();
+/// let x = MediaTypeText = "application/json".into();
+/// ```
+
+pub type MediaTypeText = String;
