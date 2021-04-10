23e568602ec8be1c7451e689ff03eec0380ed917
Joel Parker Henderson <joel@joelparkerhenderson.com>
2021-04-10 16:49:02 -0700

    Add MediaTypeSupertype

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 pub use self::grammar::noun::Noun;
 pub use self::grammar::verb::Verb;
+pub use self::media_type::media_type_supertype::MediaTypeSupertype;
+pub use self::media_type::media_type_text::MediaTypeText;
 pub use self::media_type::media_type_tree::MediaTypeTree;
 
 pub mod grammar {
@@ -8,5 +10,7 @@ pub mod grammar {
 }
 
 pub mod media_type {
+    pub mod media_type_supertype;
+    pub mod media_type_text;
     pub mod media_type_tree;
 }
---
+++ src/media_type/media_type_supertype.rs
@@ -0,0 +1,13 @@
+/// Media type supertype
+///
+/// # Examples
+///
+/// ```
+/// let x: MediaTypeSupertype = "text".into();
+/// let x: MediaTypeSupertype = "image".into();
+/// let x: MediaTypeSupertype = "audio".into();
+/// let x: MediaTypeSupertype = "video".into();
+/// let x: MediaTypeSupertype = "application".into();
+/// ```
+
+pub type MediaTypeSupertype = String;
