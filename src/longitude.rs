62489d507b5977643597a31c5b71557f380812f1
Joel Parker Henderson <joel@joelparkerhenderson.com>
2022-03-23 21:25:38 +0000

    Add longitude

---
+++ src/longitude.rs
@@ -0,0 +1,105 @@
+//! # Longitude
+//! 
+//! See the latitude documentation.
+
+//// Longitude as Decimal Degree (DD)
+
+pub type LongitudeAsDecimalDegreeAsTypeI8 = i8;
+pub type LongitudeAsDecimalDegreeAsTypeI16 = i16;
+pub type LongitudeAsDecimalDegreeAsTypeI32 = i32;
+pub type LongitudeAsDecimalDegreeAsTypeI64 = i64;
+pub type LongitudeAsDecimalDegreeAsTypeI128 = i128;
+pub type LongitudeAsDecimalDegreeAsTypeISize = isize;
+pub type LongitudeAsDecimalDegreeAsTypeU8 = u8;
+pub type LongitudeAsDecimalDegreeAsTypeU16 = u16;
+pub type LongitudeAsDecimalDegreeAsTypeU32 = u32;
+pub type LongitudeAsDecimalDegreeAsTypeU64 = u64;
+pub type LongitudeAsDecimalDegreeAsTypeU128 = u128;
+pub type LongitudeAsDecimalDegreeAsTypeUSize = usize;
+pub type LongitudeAsDecimalDegreeAsTypeF32 = f32;
+pub type LongitudeAsDecimalDegreeAsTypeF64 = f64;
+
+//// Longitude as Degree Minute (DM) with Degree
+
+pub type LongitudeAsDegreeMinuteWithDegreeAsTypeI8 = i8;
+pub type LongitudeAsDegreeMinuteWithDegreeAsTypeI16 = i16;
+pub type LongitudeAsDegreeMinuteWithDegreeAsTypeI32 = i32;
+pub type LongitudeAsDegreeMinuteWithDegreeAsTypeI64 = i64;
+pub type LongitudeAsDegreeMinuteWithDegreeAsTypeI128 = i128;
+pub type LongitudeAsDegreeMinuteWithDegreeAsTypeISize = isize;
+pub type LongitudeAsDegreeMinuteWithDegreeAsTypeU8 = u8;
+pub type LongitudeAsDegreeMinuteWithDegreeAsTypeU16 = u16;
+pub type LongitudeAsDegreeMinuteWithDegreeAsTypeU32 = u32;
+pub type LongitudeAsDegreeMinuteWithDegreeAsTypeU64 = u64;
+pub type LongitudeAsDegreeMinuteWithDegreeAsTypeU128 = u128;
+pub type LongitudeAsDegreeMinuteWithDegreeAsTypeUSize = usize;
+pub type LongitudeAsDegreeMinuteWithDegreeAsTypeF32 = f32;
+pub type LongitudeAsDegreeMinuteWithDegreeAsTypeF64 = f64;
+
+//// Longitude as Degree Minute (DM) with Minute
+
+pub type LongitudeAsDegreeMinuteWithMinuteAsTypeI8 = i8;
+pub type LongitudeAsDegreeMinuteWithMinuteAsTypeI16 = i16;
+pub type LongitudeAsDegreeMinuteWithMinuteAsTypeI32 = i32;
+pub type LongitudeAsDegreeMinuteWithMinuteAsTypeI64 = i64;
+pub type LongitudeAsDegreeMinuteWithMinuteAsTypeI128 = i128;
+pub type LongitudeAsDegreeMinuteWithMinuteAsTypeISize = isize;
+pub type LongitudeAsDegreeMinuteWithMinuteAsTypeU8 = u8;
+pub type LongitudeAsDegreeMinuteWithMinuteAsTypeU16 = u16;
+pub type LongitudeAsDegreeMinuteWithMinuteAsTypeU32 = u32;
+pub type LongitudeAsDegreeMinuteWithMinuteAsTypeU64 = u64;
+pub type LongitudeAsDegreeMinuteWithMinuteAsTypeU128 = u128;
+pub type LongitudeAsDegreeMinuteWithMinuteAsTypeUSize = usize;
+pub type LongitudeAsDegreeMinuteWithMinuteAsTypeF32 = f32;
+pub type LongitudeAsDegreeMinuteWithMinuteAsTypeF64 = f64;
+
+//// Longitude as Degree Minute Second (DMS) with Degree
+
+pub type LongitudeAsDegreeMinuteSecondWithDegreeAsTypeI8 = i8;
+pub type LongitudeAsDegreeMinuteSecondWithDegreeAsTypeI16 = i16;
+pub type LongitudeAsDegreeMinuteSecondWithDegreeAsTypeI32 = i32;
+pub type LongitudeAsDegreeMinuteSecondWithDegreeAsTypeI64 = i64;
+pub type LongitudeAsDegreeMinuteSecondWithDegreeAsTypeI128 = i128;
+pub type LongitudeAsDegreeMinuteSecondWithDegreeAsTypeISize = isize;
+pub type LongitudeAsDegreeMinuteSecondWithDegreeAsTypeU8 = u8;
+pub type LongitudeAsDegreeMinuteSecondWithDegreeAsTypeU16 = u16;
+pub type LongitudeAsDegreeMinuteSecondWithDegreeAsTypeU32 = u32;
+pub type LongitudeAsDegreeMinuteSecondWithDegreeAsTypeU64 = u64;
+pub type LongitudeAsDegreeMinuteSecondWithDegreeAsTypeU128 = u128;
+pub type LongitudeAsDegreeMinuteSecondWithDegreeAsTypeUSize = usize;
+pub type LongitudeAsDegreeMinuteSecondWithDegreeAsTypeF32 = f32;
+pub type LongitudeAsDegreeMinuteSecondWithDegreeAsTypeF64 = f64;
+
+//// Longitude as Degree Minute Second (DMS) with Minute
+
+pub type LongitudeAsDegreeMinuteSecondWithMinuteAsTypeI8 = i8;
+pub type LongitudeAsDegreeMinuteSecondWithMinuteAsTypeI16 = i16;
+pub type LongitudeAsDegreeMinuteSecondWithMinuteAsTypeI32 = i32;
+pub type LongitudeAsDegreeMinuteSecondWithMinuteAsTypeI64 = i64;
+pub type LongitudeAsDegreeMinuteSecondWithMinuteAsTypeI128 = i128;
+pub type LongitudeAsDegreeMinuteSecondWithMinuteAsTypeISize = isize;
+pub type LongitudeAsDegreeMinuteSecondWithMinuteAsTypeU8 = u8;
+pub type LongitudeAsDegreeMinuteSecondWithMinuteAsTypeU16 = u16;
+pub type LongitudeAsDegreeMinuteSecondWithMinuteAsTypeU32 = u32;
+pub type LongitudeAsDegreeMinuteSecondWithMinuteAsTypeU64 = u64;
+pub type LongitudeAsDegreeMinuteSecondWithMinuteAsTypeU128 = u128;
+pub type LongitudeAsDegreeMinuteSecondWithMinuteAsTypeUSize = usize;
+pub type LongitudeAsDegreeMinuteSecondWithMinuteAsTypeF32 = f32;
+pub type LongitudeAsDegreeMinuteSecondWithMinuteAsTypeF64 = f64;
+
+//// Longitude as Degree Minute Second (DMS) with Second
+
+pub type LongitudeAsDegreeMinuteSecondWithSecondAsTypeI8 = i8;
+pub type LongitudeAsDegreeMinuteSecondWithSecondAsTypeI16 = i16;
+pub type LongitudeAsDegreeMinuteSecondWithSecondAsTypeI32 = i32;
+pub type LongitudeAsDegreeMinuteSecondWithSecondAsTypeI64 = i64;
+pub type LongitudeAsDegreeMinuteSecondWithSecondAsTypeI128 = i128;
+pub type LongitudeAsDegreeMinuteSecondWithSecondAsTypeISize = isize;
+pub type LongitudeAsDegreeMinuteSecondWithSecondAsTypeU8 = u8;
+pub type LongitudeAsDegreeMinuteSecondWithSecondAsTypeU16 = u16;
+pub type LongitudeAsDegreeMinuteSecondWithSecondAsTypeU32 = u32;
+pub type LongitudeAsDegreeMinuteSecondWithSecondAsTypeU64 = u64;
+pub type LongitudeAsDegreeMinuteSecondWithSecondAsTypeU128 = u128;
+pub type LongitudeAsDegreeMinuteSecondWithSecondAsTypeUSize = usize;
+pub type LongitudeAsDegreeMinuteSecondWithSecondAsTypeF32 = f32;
+pub type LongitudeAsDegreeMinuteSecondWithSecondAsTypeF64 = f64;
