1a5580bc7e90abac45f508badcc93eaf3bf7e956
Joel Parker Henderson <joel@joelparkerhenderson.com>
2022-03-25 15:34:01 +0000

    Fix missing "As"

--- src/ampere.rs
+++ src/ampere.rs
@@ -4,7 +4,7 @@
 //! 
 //! ```rust
 //! # use ::typeables::ampere::*;
-//! let x = AmpereAsStructF64(1);
+//! let x = AmpereAsStructF64(1.0);
 //! ```
 //! 
 //! https://wikipedia.org/wiki/Ampere
@@ -16,32 +16,32 @@
 
 //// Ampere
 
-pub struct AmpereStructI8(pub i8);
-pub struct AmpereStructI16(pub i16);
-pub struct AmpereStructI32(pub i32);
-pub struct AmpereStructI64(pub i64);
-pub struct AmpereStructI128(pub i128);
-pub struct AmpereStructISize(pub isize);
-pub struct AmpereStructU8(pub u8);
-pub struct AmpereStructU16(pub u16);
-pub struct AmpereStructU32(pub u32);
-pub struct AmpereStructU64(pub u64);
-pub struct AmpereStructU128(pub u128);
-pub struct AmpereStructUSize(pub usize);
-pub struct AmpereStructF32(pub f32);
-pub struct AmpereStructF64(pub f64);
+pub struct AmpereAsStructI8(pub i8);
+pub struct AmpereAsStructI16(pub i16);
+pub struct AmpereAsStructI32(pub i32);
+pub struct AmpereAsStructI64(pub i64);
+pub struct AmpereAsStructI128(pub i128);
+pub struct AmpereAsStructISize(pub isize);
+pub struct AmpereAsStructU8(pub u8);
+pub struct AmpereAsStructU16(pub u16);
+pub struct AmpereAsStructU32(pub u32);
+pub struct AmpereAsStructU64(pub u64);
+pub struct AmpereAsStructU128(pub u128);
+pub struct AmpereAsStructUSize(pub usize);
+pub struct AmpereAsStructF32(pub f32);
+pub struct AmpereAsStructF64(pub f64);
 
-pub type AmpereTypeI8 = i8;
-pub type AmpereTypeI16 = i16;
-pub type AmpereTypeI32 = i32;
-pub type AmpereTypeI64 = i64;
-pub type AmpereTypeI128 = i128;
-pub type AmpereTypeISize = isize;
-pub type AmpereTypeU8 = u8;
-pub type AmpereTypeU16 = u16;
-pub type AmpereTypeU32 = u32;
-pub type AmpereTypeU64 = u64;
-pub type AmpereTypeU128 = u128;
-pub type AmpereTypeUSize = usize;
-pub type AmpereTypeF32 = f32;
-pub type AmpereTypeF64 = f64;
+pub type AmpereAsTypeI8 = i8;
+pub type AmpereAsTypeI16 = i16;
+pub type AmpereAsTypeI32 = i32;
+pub type AmpereAsTypeI64 = i64;
+pub type AmpereAsTypeI128 = i128;
+pub type AmpereAsTypeISize = isize;
+pub type AmpereAsTypeU8 = u8;
+pub type AmpereAsTypeU16 = u16;
+pub type AmpereAsTypeU32 = u32;
+pub type AmpereAsTypeU64 = u64;
+pub type AmpereAsTypeU128 = u128;
+pub type AmpereAsTypeUSize = usize;
+pub type AmpereAsTypeF32 = f32;
+pub type AmpereAsTypeF64 = f64;
